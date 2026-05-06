84942e116cd10f3322a277c67e5606a29fc76fe1
giovan48 <giovan48@hotmail.com>
2026-05-06 13:34:02 -0700

    update

---
+++ linux/cheatsheet.sh
@@ -0,0 +1,12 @@
+#Create user
+sudo adduser gal02
+
+#Add user to sudo group
+sudo usermod -aG sudo gal02
+
+#timestampt
+#sharedfolder ubuntu
+#sharedfolder kali
+#sharedfolder windows
+#mount in home directory
+#sudo mount -t vboxsf sharedgal02 /mnt
