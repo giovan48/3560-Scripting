4e7bdeeccb38a2b21d9a4978bea34af761b6005f
giovan48 <giovan48@hotmail.com>
2026-05-10 12:42:57 -0700

    update 10052026

--- bash/ch0/quickref.sh
+++ bash/ch0/quickref.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Cybersecurity Ops with bash
 
-#1- grep -> search the contents of files for a specific pattern
+#0 - Quick reference commands using in class and labs
+##- grep -> search the contents of files for a specific pattern
     #-c #count the number of lines that match the pattern
     #-E #use extended regular expressions for more complex pattern matching
     #-i #ignore case when searching for the pattern
@@ -14,6 +15,74 @@
     #-H #display the filename for each match
     #-l #display only the filenames that contain the pattern
     #-L #display only the filenames that do not contain the pattern
+
+## files and directories comparisson commands
+    #-d #check if a directory exists
+    #-e #check if a file exists
+    #-r #check if a file exists and is readable
+    #-w #check if a file exists and is writable
+    #-x #check if a file exists and is executable
+
+## literal comparisson commands
+    #-eq #check if two strings are equal
+    #-ne #check if two strings are not equal
+    #-gt #check if a string is greater than another string
+    #-lt #check if a string is less than another string
+    #-ge #check if a string is greater than or equal to another string
+    #-le #check if a string is less than or equal to another string
+    #Testing  strings variables using [[ command ]]
+        #71 echo "enter str1"
+        #72 read str1
+        #73 echo "enter str2"
+        #74 read str2
+        #75 echo "Str1 is: $str1"
+        #76 echo "Str2 is: $str2"
+        #77
+        #78 if [[ $str1 < $str2 ]]
+        #79 then    echo "value is too small"
+        #80 fi
+
+## numerical comparisson commands
+    #-eq #check if two numbers are equal
+    #-ne #check if two numbers are not equal
+    #-gt #check if a number is greater than another number
+    #-lt #check if a number is less than another number
+    #-ge #check if a number is greater than or equal to another number
+    #-le #check if a number is less than or equal to another number
+    #Testing  numerical variables using (( )) command
+        #71 echo "enter num1"
+        #72 read num1
+        #73 echo "enter num2"
+        #74 read num2
+        #75 echo "Num1 is: $num1"
+        #76 echo "Num2 is: $num2"
+        #77
+        #78 if (( num1 < 50 ))
+        #79 then    echo "value is too small"
+        #80 fi
+
+## command chaining and conditional executions
+    #; #execute multiple commands sequentially, regardless of the success or failure of the previous command
+        #cd fakefolder ; ls # This will attempt to change to a non-existent directory and then list the contents of the current directory, regardless of whether the cd command succeeded or failed
+    #&& #execute the next command only if the previous command succeeded (exit status 0
+        #cd /home && ls # This will change to the /home directory and then list its contents only if the cd command succeeded
+        #mkdir testfolder && cd testfolder # This will create a new directory named testfolder and then change into that directory only if the mkdir command succeeded
+    #|| #execute the next command only if the previous command failed (non-zero exit status)
+        #cd fakefolder || echo "cd failed" # This will attempt to change to a non-existent directory and if it fails, it will print "cd failed"
+        #ping google.com || echo "Network appears down" # This will attempt to ping google.com and if it fails, it will print "Network appears down"
+
+
+
+
+
+
+
+
+
+
+
+
+
 #2- find -> search the system for files
     #-exec #execute specified command for each file found
     #-name #search by filename
--- bash/ch2/commands.sh
+++ bash/ch2/commands.sh
@@ -2,6 +2,11 @@
 #chapter 2: Bash Primer
 #This script contains a collection of commonly used Linux commands
 
+## Shortcuts
+#To show numbers lines in ubuntu terminal, alt + shift + 3  | esc + #
+
+
+
 #Output Commands######
 echo "Hello world" #This is a comment in bash. Comments are ignored by the shell and are used to provide explanations or notes about the code.
 printf "Hello World\n" #The printf command is used to format and print text to the terminal. The \n is a newline character that moves the cursor to the next line after printing "Hello World".
@@ -21,5 +26,28 @@ echo "My name is $firstname $lastname"
     #output: My name is $firstname $lastname
 
 #Storing the output of a command in a variable
-current_date=$(date) # The date command is executed and its output is stored in the current
-echo "Current date is $current_date"
+#VARIABLE=$(command)
+#current date and time
+    current_date=$(date) # The date command is executed and its output is stored in the current
+    echo "Current date is $current_date"
+#current user
+    current_user=$(whoami) # The whoami command is executed and its output is stored in the current_user variable
+    echo "This is the current user: $current_user" | tee user.txt
+#current directory
+    current_dir=$(pwd) # The pwd command is executed and its output is stored in the current_dir variable
+    echo "This is the current directory: $current_dir"
+#current hostname
+    current_hostname=$(hostname) # The hostname command is executed and its output is stored in the current_hostname variable
+    echo "This is the current hostname: $current_hostname"
+
+#Parameters in bash
+#Positional parameters
+    echo "Parameter -0- is: $0" # The $0 variable contains the name of the script being executed
+    echo "Parameter -1- is: $1" # The $1 variable contains the first argument passed to the script
+    echo "Parameter -2- is: $2" # The $2 variable contains the secong parameter passed to the script
+    echo "Parameter -#- is: $#"  # The $# variable contains the number of arguments passed to the script
+
+#input
+    echo "enter code "
+    read code
+    echo "This is the code: $code"
